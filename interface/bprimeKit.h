d15463786afc2c485b871db121386abebe527fe7
Enoch Chen <enochnotsocool@gmal.com>
2015-05-27 23:56:06 +0800

    Reformatted with enums and and comment blocks

--- interface/bprimeKit.h
+++ interface/bprimeKit.h
@@ -1,11 +1,26 @@
+/*******************************************************************************
+ *
+ *  Filename    : bprimeKit.h
+ *  Description : The class for operating bprime kit
+ *
+*******************************************************************************/
 
 #ifndef __BPRIMEKIT_H__
 #define __BPRIMEKIT_H__
 
-// ROOT Libraries 
+//------------------------------------------------------------------------------ 
+//   Standard template libraries 
+//------------------------------------------------------------------------------ 
+#include <map>
+
+//------------------------------------------------------------------------------ 
+//   Required root libraries 
+//------------------------------------------------------------------------------ 
 #include "TTree.h"
 
-// CMSSW libraries 
+//------------------------------------------------------------------------------ 
+//   CMS Software libraries 
+//------------------------------------------------------------------------------ 
 #include "FWCore/Framework/interface/Frameworkfwd.h"
 #include "FWCore/Framework/interface/EDAnalyzer.h"
 #include "FWCore/Framework/interface/Event.h"
@@ -17,69 +32,91 @@
 
 #include "HLTrigger/HLTcore/interface/HLTConfigProvider.h"
 
-// Custom Classes 
+//------------------------------------------------------------------------------ 
+//  Helper custom classes 
+//------------------------------------------------------------------------------ 
 #include "MyAna/bprimeKit/interface/format.h"
 #include "MyAna/bprimeKit/interface/TriggerBooking.h"
 #include "MyAna/bprimeKit/interface/objectSelector.h"
 
-// Standard template library
-#include <map>
 
+
+//------------------------------------------------------------------------------ 
+//   Global typedefs, enums and macros 
+//------------------------------------------------------------------------------ 
+typedef std::vector< edm::Handle< edm::ValueMap<reco::IsoDeposit> > >   IsoDepositMaps;
+typedef std::vector< edm::Handle< edm::ValueMap<double> > >             IsoDepositVals;
 #define MAX_LEPCOLLECTIONS 3
 #define MAX_PHOCOLLECTIONS 3
 #define MAX_JETCOLLECTIONS 3
 
+
+//------------------------------------------------------------------------------ 
+//   BprimeKit definitions 
+//------------------------------------------------------------------------------ 
 class bprimeKit : public edm::EDAnalyzer {
 public:
    explicit bprimeKit( const edm::ParameterSet& ) ;
    ~bprimeKit();
 
 private:
+   //-----Methods required by plugin------------------------------------
    virtual void beginJob() ;
    virtual void analyze( const edm::Event&, const edm::EventSetup& ) ;
    virtual void endJob() ;
-   /// Private methods 
-   void fillPhoton( const edm::Event&, const edm::EventSetup& ) ;
-
    virtual void beginRun( edm::Run const& iRun, edm::EventSetup const& iSetup );
    virtual void endRun( edm::Run const&, edm::EventSetup const& );
 
+   //-----Custom helper methods----------------------------------------
+   void fillPhoton       ( const edm::Event&, const edm::EventSetup& ) ;
+   // void fillMuons     ( const edm::Event&, const edm::EventSetup& ) ;
+   // void fillElectrons ( const edm::Event&, const edm::EventSetup& ) ;
+   // void fillTaus      ( const edm::Event&, const edm::EventSetup& ) ;
+   // void fillJets      ( const edm::Event&, const edm::EventSetup& ) ;
 
-   TTree* root;
-   EvtInfoBranches EvtInfo;
-   GenInfoBranches GenInfo;
-   LepInfoBranches LepInfo[MAX_LEPCOLLECTIONS];
-   PhotonInfoBranches PhotonInfo[MAX_PHOCOLLECTIONS];
-   VertexInfoBranches VertexInfo;
-   JetInfoBranches JetInfo[MAX_JETCOLLECTIONS];
-   PairInfoBranches PairInfo;
+   //-----NTuple file interaction variables----------------------
+   TTree*                root                           ;
+   EvtInfoBranches       EvtInfo                        ;
+   GenInfoBranches       GenInfo                        ;
+   LepInfoBranches       LepInfo   [MAX_LEPCOLLECTIONS] ;
+   PhotonInfoBranches    PhotonInfo[MAX_PHOCOLLECTIONS] ;
+   JetInfoBranches       JetInfo   [MAX_JETCOLLECTIONS] ;
+   VertexInfoBranches    VertexInfo                     ;
+   PairInfoBranches      PairInfo                       ;
+
+   std::vector<std::string> lepcollections_;
+   std::vector<std::string> phocollections_;
+   std::vector<std::string> jetcollections_;
+   std::vector<int> jettype_;//true for pf jets, false for calo
    
-   std::vector<edm::InputTag> muonlabel_;
-   std::vector<edm::InputTag> eleclabel_;
-   std::vector<edm::InputTag> taulabel_;
-   std::vector<edm::InputTag> pholabel_;
-   std::vector<edm::InputTag> jetlabel_;
-   std::vector<edm::InputTag> metlabel_;
-   std::vector<edm::InputTag> pfmetlabel_;
-   std::vector<edm::InputTag> pfjetlabel_;
-   std::vector<edm::InputTag> genlabel_;
-   std::vector<edm::InputTag> hltlabel_;
-   std::vector<edm::InputTag> pathltlabel_;
-   std::vector<edm::InputTag> offlinePVlabel_;
-   std::vector<edm::InputTag> offlinePVBSlabel_;
-   std::vector<edm::InputTag> offlineBSlabel_;
-   std::vector<edm::InputTag> tracklabel_;
-   std::vector<edm::InputTag> dcslabel_;
-   std::vector<edm::InputTag> genevtlabel_;
-   std::vector<edm::InputTag> gtdigilabel_;
-   std::vector<edm::InputTag> rhocorrectionlabel_;
-   std::vector<edm::InputTag> sigmaLabel_;
-   std::vector<edm::InputTag> puInfoLabel_;
+   //-----Plugin interaction variables---------------------------
+   std::vector<edm::InputTag>  muonlabel_           ;
+   std::vector<edm::InputTag>  eleclabel_           ;
+   std::vector<edm::InputTag>  taulabel_            ;
+   std::vector<edm::InputTag>  pholabel_            ;
+   std::vector<edm::InputTag>  jetlabel_            ;
+   std::vector<edm::InputTag>  metlabel_            ;
+   std::vector<edm::InputTag>  pfmetlabel_          ;
+   std::vector<edm::InputTag>  pfjetlabel_          ;
+   std::vector<edm::InputTag>  genlabel_            ;
+   std::vector<edm::InputTag>  hltlabel_            ;
+   std::vector<edm::InputTag>  pathltlabel_         ;
+   std::vector<edm::InputTag>  offlinePVlabel_      ;
+   std::vector<edm::InputTag>  offlinePVBSlabel_    ;
+   std::vector<edm::InputTag>  offlineBSlabel_      ;
+   std::vector<edm::InputTag>  tracklabel_          ;
+   std::vector<edm::InputTag>  dcslabel_            ;
+   std::vector<edm::InputTag>  genevtlabel_         ;
+   std::vector<edm::InputTag>  gtdigilabel_         ;
+   std::vector<edm::InputTag>  rhocorrectionlabel_  ;
+   std::vector<edm::InputTag>  sigmaLabel_          ;
+   std::vector<edm::InputTag>  puInfoLabel_         ;
+   edm::InputTag               conversionsInputTag_ ;
+   edm::InputTag               rhoIsoInputTag       ;
+   std::vector<edm::InputTag>  isoValInputTags_     ;
 
+   //----- Type independent helper variables------------------------------
    // Add 2012 EID simple-cut-based
-   edm::InputTag               conversionsInputTag_;
-   edm::InputTag               rhoIsoInputTag;
-   std::vector<edm::InputTag>  isoValInputTags_;
    PFIsolationEstimator isolatorR03;
    PFIsolationEstimator isolatorR04;
    PFIsolationEstimator PhotonisolatorR03;
@@ -95,11 +132,6 @@ private:
 
    edm::EDGetTokenT<pat::PackedCandidateCollection> pfToken_;
 
-   std::vector<std::string> lepcollections_;
-   std::vector<std::string> phocollections_;
-   std::vector<std::string> jetcollections_;
-   std::vector<int> jettype_;//true for pf jets, false for calo
-
    int pairColl_;//which lepton collection to use for pairs
    bool getElectronID_;
    bool skipGenInfo_;
@@ -114,17 +146,9 @@ private:
    map < std::string, int >::iterator HLTmaplist_pr;
    HLTConfigProvider hltConfig_;
 
-
    bool isData; 
    edm::Handle<reco::VertexCollection>          VertexHandle;
    PFCandidateCollection thePfColl;
-
 };
-
-// constants, enums and typedefs for 2012 EID
-typedef std::vector< edm::Handle< edm::ValueMap<reco::IsoDeposit> > >   IsoDepositMaps;
-typedef std::vector< edm::Handle< edm::ValueMap<double> > >             IsoDepositVals;
-
-
 
 #endif /* end of include guard: __BPRIMEKIT_H__ */
--- plugins/bprimeKit_photon.cc
+++ plugins/bprimeKit_photon.cc
@@ -1,25 +1,47 @@
+/*******************************************************************************
+ *
+ *  Filename    : bprimeKit_photon.cc
+ *  Description : Fill the ntuple with photon information 
+ *  Author      : Yi-Mu "Enoch" Chen [ ensc@hep1.phys.ntu.edu.tw ]
+ *    
+ *
+*******************************************************************************/
+
 #include "MyAna/bprimeKit/interface/bprimeKit.h"
 #include "MyAna/bprimeKit/interface/GEDPhoIDTools.h"
 
 // Photon specific CMSSW PACKAGES 
 #include "DataFormats/PatCandidates/interface/Photon.h"
 
+//------------------------------------------------------------------------------ 
+//   enums and typedefs 
+//------------------------------------------------------------------------------ 
+typedef std::vector<pat::Photon>   PhotonList 
+typedef edm::Handle<PhotonList>    PhotonHandle;
+typedef std::vector<PhotonHandle>  PhotonHandleList;
 
-std::vector<edm::Handle<std::vector<pat::Photon  >>> PhoHandle;
+//------------------------------------------------------------------------------ 
+//   Helper static variables and functions 
+//------------------------------------------------------------------------------ 
+static PhotonHandleList           PhoHandle;
+static GEDPhoIDTools*             GEDIdTool; 
+static PhotonList::const_iterator it_pho ;
 
-
+//------------------------------------------------------------------------------ 
+//   bprimeKit method implementation 
+//------------------------------------------------------------------------------ 
 void bprimeKit::fillPhoton
 ( const edm::Event& iEvent ,  const edm::EventSetup& iSetup )
 {
    for( unsigned il = 0; il < pholabel_.size(); il++ ) {
-      PhoHandle.push_back( edm::Handle<std::vector<pat::Photon> >() );
+      PhoHandle.push_back( PhotonHandle() );
       iEvent.getByLabel( pholabel_[il], PhoHandle[il] );
       if( debug_ > 10 ) { cout << "photons " << il << " pholabel " << pholabel_[il] << " with " << PhoHandle[il]->size() << " entries\n"; }
    }
    
    // Setting up helper variables for photon management
    if( debug_ > 10 ) cout <<"Creating new tool" << endl;
-   GEDPhoIDTools* GEDIdTool = new GEDPhoIDTools( iEvent ) ; 
+   GEDIdTool = new GEDPhoIDTools( iEvent ) ; 
    
    for( unsigned icoll = 0; icoll < phocollections_.size(); icoll++ ) {
 
@@ -27,80 +49,62 @@ void bprimeKit::fillPhoton
       if( debug_ > 5 ) { cout << "Fill photon info, collection " << icoll << " with name " << phocollections_[icoll] << endl; }
 
       memset( &PhotonInfo[icoll], 0x00, sizeof( PhotonInfo[icoll] ) );
+      
+      if( PhoHandle.size() <= icoll ) continue ; 
+      if( debug_ > 10 ) { cout << " Photon collection size " << PhoHandle[icoll]->size() << endl; }
+      for( it_pho = PhoHandle[icoll]->begin(); it_pho != PhoHandle[icoll]->end(); it_pho++ ) {//loop over photon in collection
+         if( debug_ > 11 ) { cout << "  Size " << PhotonInfo[icoll].Size << " photon pt,eta,phi " << it_pho->pt() << "," << it_pho->eta() << "," << it_pho->phi() << endl; }
 
-      if( PhoHandle.size() > icoll ) { //have photon collection
-         if( debug_ > 10 ) { cout << " Photon collection size " << PhoHandle[icoll]->size() << endl; }
-         for( std::vector<pat::Photon>::const_iterator it_pho = PhoHandle[icoll]->begin();
-              it_pho != PhoHandle[icoll]->end(); it_pho++ ) {//loop over photon in collection
+         if ( PhotonInfo[icoll].Size >= MAX_PHOTONS ) {
+            cerr <<  "ERROR: number of photons exceeds the size of array." << endl; 
+            break;//exit(0);
+         }
 
-            if( debug_ > 11 ) { cout << "  Size " << PhotonInfo[icoll].Size << " photon pt,eta,phi " << it_pho->pt() << "," << it_pho->eta() << "," << it_pho->phi() << endl; }
-            if ( PhotonInfo[icoll].Size >= MAX_PHOTONS ) {
-               fprintf( stderr, "ERROR: number of photons exceeds the size of array.\n" );
-               break;//exit(0);
+         PhotonInfo[icoll].Pt         [PhotonInfo[icoll].Size] = it_pho->pt();
+         PhotonInfo[icoll].Eta        [PhotonInfo[icoll].Size] = it_pho->eta();
+         PhotonInfo[icoll].Phi        [PhotonInfo[icoll].Size] = it_pho->phi();
+         PhotonInfo[icoll].TrackIso   [PhotonInfo[icoll].Size] = it_pho->trackIso();
+         PhotonInfo[icoll].EcalIso    [PhotonInfo[icoll].Size] = it_pho->ecalIso();
+         PhotonInfo[icoll].HcalIso    [PhotonInfo[icoll].Size] = it_pho->hcalIso();
+         PhotonInfo[icoll].HoverE     [PhotonInfo[icoll].Size] = it_pho->hadronicOverEm();
+         PhotonInfo[icoll].SigmaIetaIeta           [PhotonInfo[icoll].Size] = it_pho->sigmaIetaIeta();
+         PhotonInfo[icoll].hadTowOverEm            [PhotonInfo[icoll].Size] = it_pho->hadTowOverEm();
+         PhotonInfo[icoll].hcalIsoConeDR04_2012    [PhotonInfo[icoll].Size] = it_pho->hcalTowerSumEtConeDR04() +
+               ( it_pho->hadronicOverEm() - it_pho->hadTowOverEm() ) * it_pho->superCluster()->energy() / cosh( it_pho->superCluster()->eta() );
+         PhotonInfo[icoll].passelectronveto[PhotonInfo[icoll].Size] = (int) it_pho->passElectronVeto() ;  
+         
+         VertexRef myVtxRef( VertexHandle, 0 );
+         
+         // TODO
+         if( debug_ > 10 ) { cout << " B4 Photon getIsolation " << endl; }
+         PhotonInfo[icoll].phoPFChIsoDR03[PhotonInfo[icoll].Size]   = GEDIdTool->SolidConeIso( 0.3 , reco::PFCandidate::h     ) ;
+         PhotonInfo[icoll].phoPFNeuIsoDR03[PhotonInfo[icoll].Size]  = GEDIdTool->SolidConeIso( 0.3 , reco::PFCandidate::h0    ) ;   
+         PhotonInfo[icoll].phoPFPhoIsoDR03[PhotonInfo[icoll].Size]  = GEDIdTool->SolidConeIso( 0.3 , reco::PFCandidate::gamma ) ;  
+
+         if( debug_ > 10 ) { cout << " Af Photon getIsolation " << endl; }
+         
+         // TODO
+         PhotonisolatorR04.fGetIsolation( &*it_pho, &thePfColl, myVtxRef, VertexHandle );
+         PhotonInfo[icoll].phoPFChIsoDR04[PhotonInfo[icoll].Size]   = PhotonisolatorR04.getIsolationCharged();
+         PhotonInfo[icoll].phoPFNeuIsoDR04[PhotonInfo[icoll].Size]  = PhotonisolatorR04.getIsolationNeutral();
+         PhotonInfo[icoll].phoPFPhoIsoDR04[PhotonInfo[icoll].Size]  = PhotonisolatorR04.getIsolationPhoton();
+
+         PhotonInfo[icoll].r9[PhotonInfo[icoll].Size]  = it_pho->r9();
+
+         if ( !isData && !skipGenInfo_ ) { //MC
+            if( debug_ > 15 ) { cout << "Getting MC information\n"; }
+            const reco::Candidate* gen = it_pho->genPhoton();
+            if ( gen != NULL ) {
+               PhotonInfo[icoll].GenPt        [PhotonInfo[icoll].Size] = gen->pt();
+               PhotonInfo[icoll].GenEta       [PhotonInfo[icoll].Size] = gen->eta();
+               PhotonInfo[icoll].GenPhi       [PhotonInfo[icoll].Size] = gen->phi();
+               PhotonInfo[icoll].GenPdgID     [PhotonInfo[icoll].Size] = gen->pdgId();
             }
 
-            if( debug_ > 11 ) { printf( "[DEBUG] Line%i\n", __LINE__ ); }
-            PhotonInfo[icoll].Pt         [PhotonInfo[icoll].Size] = it_pho->pt();
-            if( debug_ > 11 ) { printf( "[DEBUG] Line%i\n", __LINE__ ); }
-            PhotonInfo[icoll].Eta        [PhotonInfo[icoll].Size] = it_pho->eta();
-            if( debug_ > 11 ) { printf( "[DEBUG] Line%i\n", __LINE__ ); }
-            PhotonInfo[icoll].Phi        [PhotonInfo[icoll].Size] = it_pho->phi();
-            if( debug_ > 11 ) { printf( "[DEBUG] Line%i\n", __LINE__ ); }
-            PhotonInfo[icoll].TrackIso   [PhotonInfo[icoll].Size] = it_pho->trackIso();
-            if( debug_ > 11 ) { printf( "[DEBUG] Line%i\n", __LINE__ ); }
-            PhotonInfo[icoll].EcalIso    [PhotonInfo[icoll].Size] = it_pho->ecalIso();
-            if( debug_ > 11 ) { printf( "[DEBUG] Line%i\n", __LINE__ ); }
-            PhotonInfo[icoll].HcalIso    [PhotonInfo[icoll].Size] = it_pho->hcalIso();
-            if( debug_ > 11 ) { printf( "[DEBUG] Line%i\n", __LINE__ ); }
-            PhotonInfo[icoll].HoverE     [PhotonInfo[icoll].Size] = it_pho->hadronicOverEm();
-            if( debug_ > 11 ) { printf( "[DEBUG] Line%i\n", __LINE__ ); }
-            PhotonInfo[icoll].SigmaIetaIeta           [PhotonInfo[icoll].Size] = it_pho->sigmaIetaIeta();
-            if( debug_ > 11 ) { printf( "[DEBUG] Line%i\n", __LINE__ ); }
-            PhotonInfo[icoll].hadTowOverEm            [PhotonInfo[icoll].Size] = it_pho->hadTowOverEm();
-            if( debug_ > 11 ) { printf( "[DEBUG] Line%i\n", __LINE__ ); }
-            PhotonInfo[icoll].hcalIsoConeDR04_2012    [PhotonInfo[icoll].Size] = it_pho->hcalTowerSumEtConeDR04() +
-                  ( it_pho->hadronicOverEm() - it_pho->hadTowOverEm() ) * it_pho->superCluster()->energy() / cosh( it_pho->superCluster()->eta() );
-            if( debug_ > 11 ) { printf( "[DEBUG] Line%i\n", __LINE__ ); }
-            
-            if( debug_ > 11 ) { printf( "[DEBUG] Line%i\n", __LINE__ ); }
-            PhotonInfo[icoll].passelectronveto[PhotonInfo[icoll].Size] = (int) it_pho->passElectronVeto() ;  
-
-            if( debug_ > 11 ) { printf( "[DEBUG] Line%i\n", __LINE__ ); }
-            VertexRef myVtxRef( VertexHandle, 0 );
-            
-            // TODO
-            if( debug_ > 10 ) { cout << " B4 Photon getIsolation " << endl; }
-            PhotonInfo[icoll].phoPFChIsoDR03[PhotonInfo[icoll].Size]   = GEDIdTool->SolidConeIso( 0.3 , reco::PFCandidate::h     ) ;
-            PhotonInfo[icoll].phoPFNeuIsoDR03[PhotonInfo[icoll].Size]  = GEDIdTool->SolidConeIso( 0.3 , reco::PFCandidate::h0    ) ;   
-            PhotonInfo[icoll].phoPFPhoIsoDR03[PhotonInfo[icoll].Size]  = GEDIdTool->SolidConeIso( 0.3 , reco::PFCandidate::gamma ) ;  
-
-            if( debug_ > 10 ) { cout << " Af Photon getIsolation " << endl; }
-            
-            // TODO
-            PhotonisolatorR04.fGetIsolation( &*it_pho, &thePfColl, myVtxRef, VertexHandle );
-            PhotonInfo[icoll].phoPFChIsoDR04[PhotonInfo[icoll].Size]   = PhotonisolatorR04.getIsolationCharged();
-            PhotonInfo[icoll].phoPFNeuIsoDR04[PhotonInfo[icoll].Size]  = PhotonisolatorR04.getIsolationNeutral();
-            PhotonInfo[icoll].phoPFPhoIsoDR04[PhotonInfo[icoll].Size]  = PhotonisolatorR04.getIsolationPhoton();
-
-            PhotonInfo[icoll].r9[PhotonInfo[icoll].Size]  = it_pho->r9();
-
-            if ( !isData && !skipGenInfo_ ) { //MC
-               if( debug_ > 15 ) { cout << "   Getting MC information\n"; }
-               const reco::Candidate* gen = it_pho->genPhoton();
-
-               if ( gen != NULL ) {
-                  PhotonInfo[icoll].GenPt        [PhotonInfo[icoll].Size] = gen->pt();
-                  PhotonInfo[icoll].GenEta       [PhotonInfo[icoll].Size] = gen->eta();
-                  PhotonInfo[icoll].GenPhi       [PhotonInfo[icoll].Size] = gen->phi();
-                  PhotonInfo[icoll].GenPdgID     [PhotonInfo[icoll].Size] = gen->pdgId();
-               }
-
-            }
-            PhotonInfo[icoll].Size++;
-         }//loop over photons
-      }//have photons collection
+         }
+         PhotonInfo[icoll].Size++;
+      }//loop over photons
    }
-
    // Removing remaining tool 
    delete GEDIdTool ; 
 
