a1967e8994fe96b71d2c60c0eb9b1a8446c404b6
test <test>
2015-06-26 19:21:57 +0200

    New T3 storage directory

--- test/Production_MC_CMSSW74X_MiniAOD/crab.py
+++ test/Production_MC_CMSSW74X_MiniAOD/crab.py
@@ -17,7 +17,7 @@ config.Data.inputDBS = 'global'
 config.Data.splitting = 'FileBased'
 config.Data.unitsPerJob = 1
 # config.Data.totalUnits  =  50 ## Disabled to run all
-config.Data.outLFNDirBase = '/store/user/%s/CMSSW_74X_BPRIMEKIT_TESTING' % (getUsernameFromSiteDB())
+config.Data.outLFNDirBase = '/store/user/%s/CMSSW_74X_BPRIMEKIT_TESTING_2' % (getUsernameFromSiteDB())
 #config.Data.publication = True
 #config.Data.publishDataName = 'CRAB3_tutorial_May2015_MC_analysis'
 
