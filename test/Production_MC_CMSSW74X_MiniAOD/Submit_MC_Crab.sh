7b969c458102a81de0ef9a0ac304c294dc8d0df7
Enoch Chen <enochnotsocool@gmail.com>
2015-07-13 10:41:31 +0800

    Updated crab submission scripts

--- test/Production_MC_CMSSW74X_MiniAOD/Submit_MC_Crab.sh
+++ test/Production_MC_CMSSW74X_MiniAOD/Submit_MC_Crab.sh
@@ -2,16 +2,19 @@
 
 #### SET FOLDER IN Tier3 FOR SAVING OUTPUT FILES like :/dpm/phys.ntu.edu.tw/home/cms/store/user/USERID/T3_folder #### 
 
-source "/afs/cern.ch/user/y/yichen/public/NtupleControl/Master.sh"
 WORKINGPATH=`pwd`
 
-
-if [ ! -e $MASTER_DIR/MC_datasets_$USER ] ;  then 
-   echo "Error: MC_datasets_$USER doesn't exists in $MASTER_DIR"
+if [ ! -e MC_datasets_$USER ] ;  then 
+   echo "Error: MC_datasets_$USER doesn't exists!"
    exit
 fi
 
-echo -e "\n================= SOURCE CRAB ENVIRONMENT ========================"
+#-----  Helper functions  --------------------------------------------------------------------------
+function getDataLabel () {
+   echo $1 | awk -F "/" '{print $2 }' 
+}
+
+#-----  Setting up crab environment  ---------------------------------------------------------------
 export SCRAM_ARCH=slc6_amd64_gcc491
 eval `scramv1 runtime -sh`
 source /cvmfs/cms.cern.ch/crab3/crab.sh
@@ -21,19 +24,19 @@ if [[ ! -d config_files ]] ; then
    mkdir config_files
 fi 
 
-echo -e "\n================= Begin scanning MC data sets defined in MC_datasets_$USER ===================="
-for DATA in $( cat $MASTER_DIR/MC_datasets_$USER ) ;  do
+#-----  Setting up individual configuration files  -------------------------------------------------
+for DATA in $( cat MC_datasets_$USER ) ;  do
    DATALABEL=` getDataLabel $DATA `
    echo $DATALABEL
 
-   BPK_PYTHONFILE=config_files/"bprimeKit-$DATALABEL".py
+   BPK_PYTHONFILE=config_files/"bprimeKit-miniAOD-$DATALABEL".py
    CRAB_FILE=config_files/"crab-$DATALABEL".py
    OUTPUT_FILE="results-$DATALABEL".root
 
    cp ../bprimeKit_miniAOD.py            $BPK_PYTHONFILE
    sed -i "s@results.root@$OUTPUT_FILE@" $BPK_PYTHONFILE
 
-   cp $MASTER_DIR/crab_template.py            $CRAB_FILE 
+   cp ./crab_template.py                      $CRAB_FILE 
    sed -i "s@CRAB_JOB_NAME@$DATALABEL@"       $CRAB_FILE
    sed -i "s@CRAB_DATA_SET@$DATA@"            $CRAB_FILE 
    sed -i "s@BPK_PYTHONFILE@$BPK_PYTHONFILE@" $CRAB_FILE
---
+++ test/Production_MC_CMSSW74X_MiniAOD/crab_template.py
@@ -0,0 +1,23 @@
+from CRABClient.UserUtilities import config, getUsernameFromSiteDB
+config = config()
+
+config.General.requestName = 'CRAB_JOB_NAME'
+config.General.workArea = 'bprimekit_crab'
+config.General.transferOutputs = True
+config.General.transferLogs = True
+
+config.JobType.pluginName = 'Analysis'
+config.JobType.psetName = 'BPK_PYTHONFILE'
+config.JobType.inputFiles = [
+    '../dataEIDMVA' ] 
+
+config.Data.inputDataset = 'CRAB_DATA_SET'
+
+config.Data.inputDBS = 'global'
+config.Data.splitting = 'FileBased'
+config.Data.unitsPerJob = 5
+config.Data.outLFNDirBase = '/store/user/%s/BPK_PRODUCTION_TEST_2' % (getUsernameFromSiteDB())
+#config.Data.publication = True
+#config.Data.publishDataName = 'CRAB3_tutorial_May2015_MC_analysis'
+
+config.Site.storageSite = 'T3_TW_NTU_HEP'
