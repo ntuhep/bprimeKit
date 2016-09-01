cc2540c815fac92179d85e5f3230d5b81939ff76
Enoch Chen <enochnotsocool@gmail.com>
2016-09-01 19:35:22 +0200

    Update to typo

--- production/scripts/common.sh
+++ production/scripts/common.sh
@@ -23,7 +23,7 @@ function getDataProcess() {
    local data_set=$1
    if [[ $data_set == *"RunIISpring16MiniAODv2"*"reHLT"* ]]; then
       echo "MC_MiniAOD_80X_reHLT"
-   if [[ $data_set == *"RunIISpring16MiniAODv2"* ]]; then
+   elif [[ $data_set == *"RunIISpring16MiniAODv2"* ]]; then
       echo "MC_MiniAOD_80X"
    elif [[ $data_set == *"Run2016"*"-PromptReco-v"* ]]; then
       echo  "Data2016_80X"
