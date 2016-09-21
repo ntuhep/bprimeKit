c4022053e50116a1629c525e56eca2d9b4293a49
Enoch Chen <enochnotsocool@gmail.com>
2016-09-21 11:27:38 +0200

    Added helpful debugging script

--- test/DumpTriggerObjects.cc
+++ test/DumpTriggerObjects.cc
@@ -11,25 +11,48 @@
 #include "DataFormats/FWLite/interface/Handle.h"
 #include "bpkFrameWork/bprimeKit/interface/Types.h"
 
+#include <boost/program_options.hpp>
 #include "TFile.h"
+#include <string>
 #include <cstdio>
 
+using namespace std;
+namespace opt = boost::program_options;
+
 int main(int argc, char const *argv[]) {
-   if( argc != 2 ){
-      fprintf(
-         stderr,
-         "Error! Takes exactly one argument, which is the path to a miniAOD file!\n"
-      );
-      return 1;
+
+   opt::options_description desc("Options for dumping trigger object information");
+
+   desc.add_options()
+      ("file,f" , opt::value<string>(), "Path to file" )
+      ("path,p", opt::value<string>(), "HLT Path to use")
+      ("HLT,h", opt::value<string>(), "HLT Process to use (HLT for data, HLT2 for reHLT MC samples)")
+      ("help", "produces help message and exit" )
+   ;
+
+   opt::variables_map input;
+   opt::store( opt::parse_command_line(argc,argv,desc) , input );
+
+   if( input.count("help") ){
+      cout << desc << endl;
+      return 0;
    }
-   fwlite::Event ev( TFile::Open(argv[1]) );
+   if( !input.count("file") ) { cout << "file not specified!" << endl ;     return 1 ; }
+   if( !input.count("path") ) { cout << "HLT path not specified!" << endl ; return 1 ; }
+   if( !input.count("HLT") ) { cout << "HLT process not specified!" << endl ; return 1 ; }
+
+   const string filename = input["file"].as<string>();
+   const string reqpath  = input["path"].as<string>();
+   const string hlt      = input["HLT"].as<string>();
+
+   fwlite::Event ev( TFile::Open(filename.c_str()) );
    fwlite::Handle<pat::TriggerObjectStandAloneCollection> triggerobj_handle;
    fwlite::Handle<edm::TriggerResults>                    trigger_handle;
 
-   unsigned i = 0 ;
-   for( ev.toBegin() ; !ev.atEnd() && i < 100  ; ++ev ){
+   unsigned i = 1 ;
+   for( ev.toBegin() ; !ev.atEnd() && i < 100  ; ++ev , ++i ){
       triggerobj_handle.getByLabel( ev, "selectedPatTrigger" );
-      trigger_handle.getByLabel( ev, "TriggerResults" , "" , "HLT2" );
+      trigger_handle.getByLabel( ev, "TriggerResults" , "" , hlt.c_str() );
       const auto& triggernames = ev.triggerNames( *trigger_handle );
 
       fprintf( stdout , "Event [%u/%lld]\n", i , ev.size() );
@@ -37,6 +60,13 @@ int main(int argc, char const *argv[]) {
       unsigned j = 0 ;
       for( auto obj : *triggerobj_handle ){
          obj.unpackPathNames(triggernames);
+
+         bool haspath = false;
+         for( const auto& path : obj.pathNames() ){
+            if( path.find(reqpath) != std::string::npos && obj.hasPathName(path)) { haspath=true; break; }
+         }
+         if( !haspath ) { continue ; }
+
          fprintf(
             stdout, "\tTrigger object [%d] pt:%lf eta:%lf phi%lf\n",
             j, obj.pt(), obj.eta(), obj.phi()
