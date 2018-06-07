e9332d21f31891da8a13d517736f07e75905a6f3
Youying <luffy7804@gmail.com>
2018-06-07 11:58:14 +0200

    Remove EGamma 94X object setup

--- setup.sh
+++ setup.sh
@@ -4,11 +4,6 @@ echo
 echo "Welcome to the bprimekit setup script!"
 echo
 
-echo "Set the latest Egamma ID Recipes in 2017"
-git cms-merge-topic lsoffi:CMSSW_9_4_0_pre3_TnP
-echo
-
-
 echo "Set up JetToolBox"
 git clone https://github.com/cms-jet/JetToolbox.git JMEAnalysis/JetToolbox -b jetToolbox_94X_v1
 echo
