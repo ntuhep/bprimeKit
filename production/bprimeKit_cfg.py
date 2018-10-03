9c64501dbebde8e039c422aa40a1950509b15094
Youying <luffy7804@gmail.com>
2018-10-03 05:28:45 +0200

    Update MET bug fixing

--- production/bprimeKit_cfg.py
+++ production/bprimeKit_cfg.py
@@ -120,6 +120,7 @@ runMetCorAndUncFromMiniAOD (
     process,
     isData = mysetting.isData,
     fixEE2017 = mysetting.isData,
+    fixEE2017Params = {'userawPt': True, 'PtThreshold':50.0, 'MinEtaThreshold':2.65, 'MaxEtaThreshold': 3.139},
     postfix = "ModifiedMET"
     )
 
