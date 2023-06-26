21f537a65a287cb7fa73b1f561e8bc7bdb67a6f2
Youying <luffy7804@gmail.com>
2023-06-26 18:41:15 +0200

    Update minor things

--- production/submitsample.py
+++ production/submitsample.py
@@ -10,7 +10,7 @@ crabcfgformat="""
 from CRABClient.UserUtilities import config
 config = config()
 
-config.General.requestName = 'BPK_124X_{0}'
+config.General.requestName = 'BPK_130X_{0}'
 config.General.workArea = 'bprimeKit_crab'
 config.General.transferOutputs = True
 config.General.transferLogs = True
@@ -47,7 +47,7 @@ def submitsample(argv):
     parser=optparse.OptionParser()
     parser.add_option("-d","--dataset",dest='dataset',help='which dataset to run', default=None, type='string')
     parser.add_option('-s','--site',dest='site',help='which site to store output', default='T2_CH_CERN', type='string')
-    parser.add_option('-l','--lfndir',dest='lfndir',help='the storage lfn directory' , default='/store/group/phys_b2g/BprimeKit_Ntuples_CMSSW_124X', type='string')
+    parser.add_option('-l','--lfndir',dest='lfndir',help='the storage lfn directory' , default='/store/group/phys_b2g/BprimeKit_Ntuples_CMSSW_130X', type='string')
 
     opt, args = parser.parse_args( argv )
 
